922275f0cb83d8ba2c61e880d090638bc30c3db6
GrimStarlight <felcrystals@gmail.com>
2024-09-06 07:06:05 -0600

    Update soundOnStep.rb

--- soundOnStep.rb
+++ soundOnStep.rb
@@ -1,10 +1,20 @@
 class Game_Player < Game_Character
   alias original_update update
-  
+
+  def initialize(*args)
+    super(*args)
+    @last_x = self.x
+    @last_y = self.y
+  end
+
   def update
     original_update
-    if $game_map.terrain_tag($game_player.x, $game_player.y) == 2
-      pbSEPlay("SandStep", 80, 100)
+    if (self.x != @last_x || self.y != @last_y)
+      if $game_map.terrain_tag($game_player.x, $game_player.y) == 2
+        pbSEPlay("SandStep", 80, 100)
+      end
+      @last_x = self.x
+      @last_y = self.y
     end
   end
 end
