aa31649602ff9df369ce6d204eaf7c8cb6f16028
GrimStarlight <felcrystals@gmail.com>
2024-09-08 00:31:24 -0600

    Update staticSafari.rb

--- staticSafari.rb
+++ staticSafari.rb
@@ -1,8 +1,7 @@
 def static_safari_encounter(pokemon_id, level)
   pbSafariBattle(pokemon_id, level)
   
-  # Check if the battle was won (Pokémon caught)
-  if $PokemonTemp.safariState == 1  # If 1, Pokémon was caught
+  if $PokemonTemp.safariState == 1 
     pbMessage(_INTL("Gotcha! {1} was caught!",PBSpecies.getName(pokemon_id)))
     pbAddPokemon(pokemon_id, level)
 
